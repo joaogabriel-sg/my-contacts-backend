160c4fec8068d91065af9829e7a4e502e4ece26a
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-13 07:23:31 -0300

    :sparkles: add route to get contact by id

--- src/routes.js
+++ src/routes.js
@@ -5,5 +5,6 @@ const ContactController = require('./app/controllers/ContactController');
 const router = Router();
 
 router.get('/contacts', ContactController.index);
+router.get('/contacts/:id', ContactController.show);
 
 module.exports = router;
