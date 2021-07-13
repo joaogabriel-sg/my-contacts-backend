1fa403b0cb3804d5efee2d7df5f74b1c3d2cd7d8
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-13 06:28:38 -0300

    :sparkles: add contact controller

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,19 @@
+class ContactController {
+  index(req, res) {
+    res.send('Send from Contact Controller');
+  }
+
+  show() {
+  }
+
+  store() {
+  }
+
+  update() {
+  }
+
+  delete() {
+  }
+}
+
+module.exports = new ContactController();
