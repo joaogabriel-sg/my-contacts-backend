9f3c840261184a70b9070785e6a56c4efec8f0af
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-13 18:00:40 -0300

    :sparkles: add functions to update an existent contact

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -30,7 +30,7 @@ class ContactController {
     const contactExists = await ContactsRepository.findByEmail(email);
 
     if (contactExists) {
-      return res.status(400).json({ error: 'This email is already been taken' });
+      return res.status(400).json({ error: 'This e-mail is already in use' });
     }
 
     const contact = await ContactsRepository.create({
@@ -40,7 +40,33 @@ class ContactController {
     res.json(contact);
   }
 
-  update() {
+  async update(req, res) {
+    const { id } = req.params;
+    const {
+      name, email, phone, category_id,
+    } = req.body;
+
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return res.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmail && contactByEmail.id !== id) {
+      return res.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    res.json(contact);
   }
 
   async delete(req, res) {
