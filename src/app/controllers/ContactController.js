2717b6788270e1816581576fb00499a2fa668ef8
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-13 07:24:20 -0300

    :sparkles: add functions to get a contact by id

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -7,7 +7,15 @@ class ContactController {
     res.json(contacts);
   }
 
-  show() {
+  async show(req, res) {
+    const { id } = req.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(contact);
   }
 
   store() {
--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -8,11 +8,24 @@ const contacts = [
     phone: '1212121212',
     category_id: uuid(),
   },
+  {
+    id: uuid(),
+    name: 'Isabele Gonçalves',
+    email: 'isabelegoncalves@email.com',
+    phone: '1313131313',
+    category_id: uuid(),
+  },
 ];
 
 class ContactsRepository {
   findAll() {
     return new Promise((resolve) => resolve(contacts));
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
   }
 }
 
