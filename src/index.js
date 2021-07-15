907dbae1484d5b9e42ca34a928b0ba3961676324
João Gabriel <joaogabrielsg.00@gmail.com>
2021-07-15 11:09:35 -0300

    :sparkles: add error handler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -6,5 +7,10 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, req, res, next) => {
+  console.log('######## ERROR HANDLER');
+  console.log(error);
+  res.sendStatus(500);
+});
 
 app.listen(8080, () => console.log('🔥 Server started at http://localhost:8080'));
